c48fc7e2ec3cb37ca200fe74c4bd283787f3be09
navin singh dalal <navin.singh.dalal@gmail.com>
2019-11-28 14:27:33 +0530

    changes
    changes

--- UrlShotening/UrlShortner.py
+++ UrlShotening/UrlShortner.py
@@ -21,18 +21,19 @@ Refere this tutorial https://www.codementor.io/garethdwyer/building-a-crud-appli
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
+import psycopg2
 from random import randint
 app = Flask(__name__)
 
 
 database_file = "postgresql://postgres:$shaurya1@localhost/testdb"
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
 
 class Longshorturl(db.Model):
-    longurl = db.Column(db.String(200), unique=True, nullable=False, primary_key=True)
+    longurl = db.Column(db.String(200), unique=True, nullable=True, primary_key=True)
     shorturl = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
@@ -45,20 +46,25 @@ db.session.commit()
 
 @app.route('/', methods=["GET", "POST"])
 def home():
+    bool_longurl = True
     if request.form:
         try:
-            shorturl = 'http://bitly/'+str(randint(1000, 9999))
-            db.session.add(Longshorturl(longurl=request.form.get("longurl"), shorturl=shorturl))
+            shorturl = 'http://bitly_'+str(randint(1000, 9999))
+            longurl = request.form.get("longurl")
+            db.session.add(Longshorturl(longurl=longurl, shorturl=shorturl))
             db.session.commit()
+            all_records = Longshorturl.query.order_by(Longshorturl.longurl).all()
+            print(all_records)
+            return render_template('home.html', all_records=all_records)
         except sqlalchemy.exc.IntegrityError:
-            a=request.form.get("longurl")
-            var_shorturl = Longshorturl.query.filter_by(longurl=a).first()
-            context = {}
-            print(var_shorturl.longurl, var_shorturl.shorturl)
-            context[request.form.get("longurl")] = var_shorturl
-            print(context)
-            len_dict = len(context)
-            return render_template("home.html", var_context1=context, dictlen=len_dict)
+            db.session.rollback()
+            a = request.form.get("longurl")
+            try:
+                var_shorturl = Longshorturl.query.filter_by(longurl=a).first().shorturl
+                var_longurl = Longshorturl.query.filter_by(longurl=a).first().longurl
+            except Exception as err:
+                print('FunctionName: %s', err)
+            return render_template("home.html", var_shorturl=var_shorturl, var_longurl=var_longurl)
     return render_template("home.html")
 
 
